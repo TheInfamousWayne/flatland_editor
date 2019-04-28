a9965d55cda07dfa7f64e68988d1ed696f182ab3
Vaibhav Agrawal <theinfamouswayne@gmail.com>
2019-04-29 00:34:55 +0200

    Updates

---
+++ precache-manifest.5f5efa6592bdcf4e69ea9c56d3e5e3d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742251f3ca914895b22c78dccb1c7b56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95f000e9910c994af28a",
+    "url": "/static/css/main.27385f87.chunk.css"
+  },
+  {
+    "revision": "e266ae10f2a95928076f",
+    "url": "/static/js/2.9623777e.chunk.js"
+  },
+  {
+    "revision": "95f000e9910c994af28a",
+    "url": "/static/js/main.5444478b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
